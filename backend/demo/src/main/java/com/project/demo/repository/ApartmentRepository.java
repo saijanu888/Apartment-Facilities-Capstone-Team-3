407d988dfa901503824fe5ab5e7790f73d9f72be
saijanu888 <sai.janardhan8@gmail.com>
2024-07-25 17:26:59 +0530

    Bckend

--- backend/demo/src/main/java/com/project/demo/repository/ApartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.demo.entity.Apartment;
-
-public interface ApartmentRepository extends JpaRepository<Apartment, Long> {
-
-}
